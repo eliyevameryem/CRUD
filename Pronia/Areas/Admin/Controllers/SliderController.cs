336b0e7a0a6af058af20b3f21ab4309781189d01
unknown <bd7pcksnr@code.edu.az>
2023-11-23 22:48:31 +0300

    update

--- Pronia/Areas/Admin/Controllers/SliderController.cs
+++ Pronia/Areas/Admin/Controllers/SliderController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 using Pronia.DAL;
 using Pronia.Models;
 using Pronia.Utilities;
@@ -8,16 +10,19 @@ namespace Pronia.Areas.Admin.Controllers
     [Area("Admin")]
     public class SliderController : Controller
     {
-        AppDbContext _content;
+        AppDbContext _context;
+        private IWebHostEnvironment _env;
 
-        public SliderController(AppDbContext content)
+        public SliderController(AppDbContext context,IWebHostEnvironment env)
         {
-            _content = content;
+            _context = context;
+            _env=env;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Slider> slider = await _context.Sliders.ToListAsync();
+            return View(slider);
         }
 
         public IActionResult Create()
@@ -33,22 +38,24 @@ namespace Pronia.Areas.Admin.Controllers
                 return View();
             }
 
-            if (!slider.ImageFile.CheckFileType("image/"))
-            {
-                ModelState.AddModelError("ImageUrel", "File duzgun daxil elemediniz");
-                return View();
-            }
-            if(slider.ImageFile.CheckFileLength(200))
+            //if (!slider.ImageFile.CheckFileType("image/"))
+            //{
+            //    ModelState.AddModelError("ImageUrel", "File duzgun daxil elemediniz");
+            //    return View();
+            //}
+
+            if (slider.ImageFile.CheckFileLength(200))
             {
                 ModelState.AddModelError("ImageFile", "Maksimum 2mb olcude sekil yukleye bilersiz");
                 return View();
             }
 
+            
             string filname = slider.ImageFile.FileName;
 
             filname = Guid.NewGuid().ToString()+filname;
 
-            string path = "C:\\Users\\Asus\\Desktop\\thome\\Pronia\\wwwroot\\UploadImage\\SliderImage\\" + filname;
+            string path = _env.WebRootPath+@"/UploadImage/SliderImage" + filname;
 
 
             using (FileStream stream = new FileStream(path, FileMode.Create))
@@ -60,7 +67,10 @@ namespace Pronia.Areas.Admin.Controllers
 
 
 
+             _context.Sliders.AddAsync(slider);
+             _context.SaveChangesAsync();
 
+            return RedirectToAction(nameof(Index));
 
 
             return View();
--- Pronia/Utilities/FileExtension.cs
+++ Pronia/Utilities/FileExtension.cs
@@ -1,16 +1,39 @@
-﻿namespace Pronia.Utilities
+﻿using static NuGet.Packaging.PackagingConstants;
+
+namespace Pronia.Utilities
 {
     public static class FileExtension
     {
-        public static bool CheckFileType(this IFormFile ImageFile, string type)
-        {
-            return ImageFile.Contenttype(type);
-        }
+        //public static bool CheckFileType(this IFormFile ImageFile, string type)
+        //{
+        //    //return ImageFile.Contenttype(type);
+        //}
         public static bool CheckFileLength(this IFormFile ImageFile, int length)
         {
             return ImageFile.Length > length;
         }
+        public static string UploadFile(this IFormFile file, string envPath, string folderName)
+        {
+            string filename = Guid.NewGuid().ToString() + file.FileName;
 
-        
+            string path = Path.Combine(root, folder, filename);
+
+            using (FileStream stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            return filename;
+        }
+
+        public static void DeleteFile(this string image, string root, string folder)
+        {
+            string path = Path.Combine(root, folder, image);
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
     }
 }
